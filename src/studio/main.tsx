6d9a691fc0a823fc866b9ae88095e66ad0e4a3db
PrisacariuRobert <145539911+PrisacariuRobert@users.noreply.github.com>
2026-09-15 01:37:42 +0200

    Load appearance contract fixes after polish layers

--- src/studio/main.tsx
+++ src/studio/main.tsx
@@ -12,6 +12,7 @@ import "./apple-polish.css";
 import "./apple-pages.css";
 import "./apple-capabilities.css";
 import "./apple-brand.css";
+import "./apple-contract-fixes.css";
 import { applyAppearance, savedAppearance } from "./useAppearance";
 
 applyAppearance(savedAppearance());
